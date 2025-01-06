f1821f759b843e5cdb580e62382dccaeda3abb60
aikoolee <aikolee2023@gmail.com>
2025-01-06 10:44:01 +0700

    cart fix

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -158,15 +158,10 @@
         <h1 class="text-4xl font-bold text-white">Your Cart</h1>
     </div>
 </div>
-<div class="min-h-screen bg-[#e7e3d8] dark:bg-[#1e1a14] py-8">
+<div class="min-h-screen bg-[#e7e3d8] dark:bg-[#1e1a14] py-3">
     <div class="container mx-auto px-4 sm:px-6 lg:px-8">
 
         @if(count($menuDetails) > 0)
-            <a href="{{ url('/menu') }}" class="inline-block bg-[#d6a670] hover:bg-[#c89550] text-white font-medium py-1.5 px-3 rounded dark:bg-[#9a7f48] dark:hover:bg-[#7c6539] mb-4 text-sm">
-                Back to Menu
-            </a>
-            
-
             <div class="overflow-x-auto">
                 <!-- Tabel Cart -->
                 <table class="cart-table w-full text-center border-collapse">
